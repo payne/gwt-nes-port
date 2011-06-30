28cec043d3a599bfba3ba72ca8da7a5450f5d4ca
brad.rydzewski <brad.rydzewski@613099ce-d8ec-6989-86c3-2dc68e9d80c6>
2011-06-30 06:14:21 +0000

    Added initial port of HalfNes to GWT. Everything works, with the exception of audio and sram.

--- gwt-nes-port/src/com/gochromium/nes/client/service/MetaDataService.java
+++
@@ -1,8 +0,0 @@
-package com.gochromium.nes.client.service;
-
-import com.gochromium.nes.client.model.MetaData;
-
-public interface MetaDataService {
-
-	public MetaData findMetaData(String file);
-}
